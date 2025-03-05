2397d29620e38aa1e7cb4b592a1901ce181b2468
alex <sasha.borchenko@gmail.com>
2025-03-05 18:58:30 +0200

    implemented xor func

--- Chapter_2/Practice Problems/2_13.c
+++ Chapter_2/Practice Problems/2_13.c
@@ -14,14 +14,14 @@ int bool_or(int x, int y)
 	int result = bis(x, y);
 	return result;
 }
-/* Compute x^y using only calls to functions bis and bic */ // x^y =  
-// int bool_xor(int x, int y)
-// {
-// 	int result = bic();
-// 	return result;
-// }
+/* Compute x^y using only calls to functions bis and bic */ // x^y =
+int bool_xor(int x, int y)
+{
+	int result = bis(bic(x, y), bic(y, x));
+	return result;
+}
 int main(int argc, char const *argv[])
 {
-	printf("%.8X\n", 68 & (~100));
+	printf("%X\n", bool_xor(12, 20));
 	return 0;
 }
